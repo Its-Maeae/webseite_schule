c31c81cf96535ffc973a14c05745fc113baca964
Its-Maeae <mariusschmi1711@gmail.com>
2026-08-31 21:16:09 +0200

    fix

--- src/pages/ProjectsKlasse12.tsx
+++ src/pages/ProjectsKlasse12.tsx
@@ -1,6 +1,25 @@
-export default function ProjectsKlasse12() {
+export default function Klasse11() {
   return (
     <main className="container">
+      <div className="projekte-11">
+        <h1><div className="home-title">Klasse 12</div></h1>
+        <div className="home-text">Übersichten und Projekte aus der Klassenstufe 11</div>
+        <br></br>
+        <div className="home-links">
+          <div className="home-link-container">
+            <div className="home-link-subtext">....</div>
+            <a href="/projekte/klasse12" className="home-link">....</a>
+          </div>
+          <div className="home-link-container">
+            <div className="home-link-subtext">....</div>
+            <a href="/projekte/klasse12" className="home-link">....</a>
+          </div>
+          <div className="home-link-container">
+            <div className="home-link-subtext">....</div>
+            <a href="/projekte/klasse12" className="home-link">....</a>
+          </div>
+        </div>
+      </div>
     </main>
-  );
+  )
 }
--- src/pages/content/klasse_11/Flilius/components/FiliusGrundlagen.tsx
+++ src/pages/content/klasse_11/Flilius/components/FiliusGrundlagen.tsx
@@ -2,6 +2,10 @@ export default function FiliusGrundlagen() {
   return (
     <main className="container-components">
       <div className="heading">Filius Grundlagen</div>
+        <div className="list">• Servernamen werden großgeschrieben</div>
+        <br></br>
+      <div className="subsubheading">Simulation (Virtueller Computer)</div>
+      <div className="list">• auf dem Play Button oben kann es gestartet werden</div>
     </main>
   )
 }
